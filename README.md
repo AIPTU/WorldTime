9b6a1f99e4ade32ae0052bb293b5012ce71943d5
AIPTU <anangp808@gmail.com>
2023-06-15 23:36:16 +0700

    Support PM5

--- README.md
+++ README.md
@@ -3,45 +3,51 @@
 [![](https://poggit.pmmp.io/shield.state/WorldTime)](https://poggit.pmmp.io/p/WorldTime)
 [![](https://poggit.pmmp.io/shield.dl.total/WorldTime)](https://poggit.pmmp.io/p/WorldTime)
 
-A PocketMine-MP plugin to set time and stop time easily.
+The WorldTime plugin is a PocketMine plugin that allows you to set the time of specific worlds in your server based on a configuration file.
 
-# Features
+## Features
 
-- Customize messages when setting world time.
-- `{WORLD}` can be used to display world folder name.
-- `{TIME}` can be used to display world time.
-- Lightweight and open source ❤️
+- Set the time of individual worlds in your server.
+- Support for different time values, including predefined values like day, night, sunset, sunrise, etc.
+- Option to stop the time in a world.
+- Customizable log message when setting the time of a world.
+- Automatic generation of a new configuration file if an outdated one is provided.
 
-# Default Config
+## Configuration
+
+The plugin comes with a configuration file named `config.yml`. You can modify this file to configure the plugin behavior.
+
+The `config.yml` file contains the following options:
+
+- `config-version`: **(Do not modify)** The version of the configuration file.
+- `worlds`: An array of worlds and their settings. Each world has the following options:
+  - `time`: The desired time value for the world. It can be one of the predefined values (day, night, sunset, sunrise, noon) or a custom integer value.
+  - `stop`: *(Optional)* Set to `true` if you want to stop the time in the world.
+- `message`: The log message to display when setting the time of a world. You can use placeholders to dynamically insert values. Available placeholders: `{WORLD}`, `{TIME}`.
+
+## Usage
+
+1. Edit the `config.yml` file to specify the worlds and their time settings.
+2. Start or reload the server.
+3. The plugin will automatically load the specified worlds and set their time according to the configuration.
+
+The plugin will log a message for each world it sets the time for, using the configured log message. The `{WORLD}` and `{TIME}` placeholders in the log message will be replaced with the respective values.
+
+## Example Configuration
+
 ```yaml
----
-# Do not change this (Only for internal use)!
-config-version: 1.1
-
-# Message used when setting world time.
-# Available tags:
-# - {WORLD}: Show world folder name.
-# - {TIME}: Show world time.
-message: "Set the time of the {WORLD} world to {TIME}"
-
 worlds:
-  # World folder name.
-  world:
-    # World time to set.
+  world1:
     time: day
-    # Stop time
-    stop: true
-  # World folder name with spaces.
-  "example world":
-    time: night
     stop: false
-...
-
+  world2:
+    time: 6000
+    stop: true
+  world3:
+    time: sunset
+    stop: false
+message: 'Set the time of the {WORLD} world to {TIME}.'
 ```
-
-# Upcoming Features
-
-- Currently none planned. You can contribute or suggest for new features.
 
 # Additional Notes
 
--- src/WorldTime.php
+++ src/WorldTime.php
@@ -1,27 +1,12 @@
 <?php
 
 /*
+ * Copyright (c) 2021-2023 AIPTU
  *
- * Copyright (c) 2021 AIPTU
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
+ * @see https://github.com/AIPTU/WorldTime
  */
 
 declare(strict_types=1);
@@ -30,83 +15,127 @@ namespace aiptu\worldtime;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\world\World;
+use function assert;
+use function is_int;
+use function is_string;
+use function max;
+use function min;
 use function rename;
 use function str_replace;
 
-final class WorldTime extends PluginBase
-{
+class WorldTime extends PluginBase {
 	private const CONFIG_VERSION = 1.1;
 
-	public function onEnable(): void
-	{
+	public function onEnable() : void {
 		$this->checkConfig();
-
-		$this->loadWorld();
+		$this->loadWorlds();
 	}
 
-	public function replaceVars(string $str, array $vars): string
-	{
-		foreach ($vars as $key => $value) {
-			$str = str_replace('{' . $key . '}', (string) $value, $str);
-		}
-		return $str;
-	}
-
-	private function checkConfig(): void
-	{
+	/**
+	 * Checks and handles the plugin's configuration.
+	 */
+	private function checkConfig() : void {
 		$this->saveDefaultConfig();
 
-		if (!$this->getConfig()->exists('config-version') || ($this->getConfig()->get('config-version', self::CONFIG_VERSION) !== self::CONFIG_VERSION)) {
-			$this->getLogger()->warning('An outdated config was provided attempting to generate a new one...');
-			if (!rename($this->getDataFolder() . 'config.yml', $this->getDataFolder() . 'config.old.yml')) {
+		$config = $this->getConfig();
+		if (!$config->exists('config-version') || $config->get('config-version') !== self::CONFIG_VERSION) {
+			$this->getLogger()->warning('An outdated config was provided, attempting to generate a new one...');
+
+			$configFile = $this->getDataFolder() . 'config.yml';
+			$newConfigFile = $this->getDataFolder() . 'config.old.yml';
+
+			if (!rename($configFile, $newConfigFile)) {
 				$this->getLogger()->critical('An unknown error occurred while attempting to generate the new config');
 				$this->getServer()->getPluginManager()->disablePlugin($this);
+				return;
 			}
+
 			$this->reloadConfig();
 		}
 	}
 
-	private function loadWorld(): void
-	{
-		foreach ($this->getConfig()->get('worlds', []) as $worlds => $value) {
-			$this->getServer()->getWorldManager()->loadWorld($worlds);
+	/**
+	 * Loads worlds and sets their time based on the plugin's configuration.
+	 */
+	private function loadWorlds() : void {
+		/** @var array<string, array{time: int|string, stop: bool}> $worldsConfig */
+		$worldsConfig = $this->getConfig()->get('worlds', []);
+		$worldManager = $this->getServer()->getWorldManager();
 
-			$world = $this->getServer()->getWorldManager()->getWorldByName($worlds);
-			$time = $value['time'];
-			$stop = $value['stop'];
-			if ($world !== null) {
-				match ($time) {
-					'day' => $time = World::TIME_DAY,
-					'noon' => $time = World::TIME_NOON,
-					'sunset' => $time = World::TIME_SUNSET,
-					'night' => $time = World::TIME_NIGHT,
-					'midnight' => $time = World::TIME_MIDNIGHT,
-					'sunrise' => $time = World::TIME_SUNRISE,
-					default => $time = $this->getInteger($time, 0),
-				};
-				$world->setTime($time);
-				if ($stop) {
-					$world->stopTime();
-				}
-				$message = $this->getConfig()->get('message', 'Set the time of the {WORLD} world to {TIME}');
-				$this->getLogger()->notice($this->replaceVars($message, [
-					'WORLD' => $world->getFolderName(),
-					'TIME' => $time,
-				]));
+		foreach ($worldsConfig as $worldName => $worldData) {
+			if (!$worldManager->isWorldLoaded($worldName)) {
+				$worldManager->loadWorld($worldName);
 			}
+
+			$world = $worldManager->getWorldByName($worldName);
+			if ($world === null) {
+				$this->getLogger()->warning("Failed to load world '{$worldName}'.");
+				continue;
+			}
+
+			$time = $this->getTimeValue($worldData['time']);
+			$stopTime = $worldData['stop'];
+
+			$world->setTime($time);
+			if ($stopTime) {
+				$world->stopTime();
+			}
+
+			$message = $this->getConfig()->get('message', 'Set the time of the {WORLD} world to {TIME}');
+			assert(is_string($message), 'Invalid message format.');
+			$replacements = [
+				'WORLD' => $world->getFolderName(),
+				'TIME' => $time,
+			];
+			$this->getLogger()->notice($this->replaceVars($message, $replacements));
 		}
 	}
 
-	private function getInteger(string $value, int $min = 30000000, int $max = -30000000): int
-	{
-		$i = (int) $value;
+	/**
+	 * Converts a time string or integer to a valid World time value.
+	 *
+	 * @param mixed $time the time value to convert
+	 *
+	 * @return int the World time value
+	 */
+	private function getTimeValue($time) : int {
+		return match ($time) {
+			'day' => World::TIME_DAY,
+			'noon' => World::TIME_NOON,
+			'sunset' => World::TIME_SUNSET,
+			'night' => World::TIME_NIGHT,
+			'midnight' => World::TIME_MIDNIGHT,
+			'sunrise' => World::TIME_SUNRISE,
+			default => is_int($time) ? $this->getBoundedInteger($time, 0) : 0,
+		};
+	}
 
-		if ($i < $min) {
-			$i = $min;
-		} elseif ($i > $max) {
-			$i = $max;
+	/**
+	 * Ensures the given integer value falls within the specified bounds.
+	 *
+	 * @param int $value the integer value to check
+	 * @param int $min   the minimum allowed value
+	 * @param int $max   the maximum allowed value
+	 *
+	 * @return int the bounded integer value
+	 */
+	private function getBoundedInteger(int $value, int $min, int $max = -30000000) : int {
+		return max(min($value, $min), $max);
+	}
+
+	/**
+	 * Replaces variables in a string with corresponding values from an array.
+	 *
+	 * @param string $str  the input string
+	 * @param array  $vars an associative array of variables and their values
+	 *
+	 * @return string the modified string
+	 */
+	private function replaceVars(string $str, array $vars) : string {
+		foreach ($vars as $key => $value) {
+			$str = str_replace('{' . $key . '}', (string) $value, $str);
 		}
 
-		return $i;
+		return $str;
 	}
 }
